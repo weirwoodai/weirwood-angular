989f9b088bc799333b923a552736c63ddee857fc
Ricard_MBP16 <rikitzzz@gmail.com>
2020-11-04 11:49:11 +0100

    feat: added links to products, about and contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 import { TickersComponent } from './pages/tickers/tickers.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'tickers', component: TickersComponent },
-  {
-    path: 'login',
-    loadChildren: () => import('./user/user.module').then((m) => m.UserModule)
-  }
+  { path: 'products', component: ProductsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+  // { path: 'tickers', component: TickersComponent },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./user/user.module').then((m) => m.UserModule)
+  // }
 ];
 
 @NgModule({
